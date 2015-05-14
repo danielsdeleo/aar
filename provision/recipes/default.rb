a2c8cbb24a08625921d753cde36e8320465116c3
danielsdeleo <dan@getchef.com>
2015-05-14 09:06:48 -0700

    Add provision cookbook

---
+++ provision/recipes/default.rb
@@ -0,0 +1,20 @@
+
+context = ChefDK::ProvisioningData.context
+
+with_driver 'vagrant:~/.vagrant.d/boxes' do
+
+  options = {
+    vagrant_options: { 'vm.box' => 'opscode-ubuntu-14.04' },
+    convergence_options: context.convergence_options
+  }
+
+
+  machine context.node_name do
+    machine_options(options)
+
+    # This forces a chef run every time, which is sensible for `chef provision`
+    # use cases.
+    converge(true)
+    action(context.action)
+  end
+end
